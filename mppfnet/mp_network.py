e56c24702b5ec8efb43547c6c6f87a1e450f97b8
Martin Zellner <martin.zellner@martinzellner.eu>
2016-05-04 13:40:00 +0200

    implemented setting and getting of load profiles

--- mppfnet/mp_network.py
+++ mppfnet/mp_network.py
@@ -1,12 +1,18 @@
 import pfnet
-
+from datetime import datetime
+from . import load_profile
+from . import solar_profile
 
 class MPNetwork():
 
     network = dict()
-    timesteps = 1
+    timesteps = 8760
+    start_date = datetime.strptime("1.1.2016", "%d.%m.%Y")
+    end_date = datetime.strptime("1.1.2017", "%d.%m.%Y")
+    load_profile_map = dict()
+    solar_profile_map = dict()
 
-    def __init__(self, timesteps=1):
+    def __init__(self, timesteps=8760):
         self.timesteps = timesteps
         for i in range(self.timesteps):
             self.network[i] = pfnet.Network()
@@ -50,5 +56,23 @@ class MPNetwork():
     def get_load(self, index, timestep):
         return self.network[timestep].get_load(index)
 
+    def generate_load_profiles(self):
+        for i in range(self.network[0].num_loads):
+            self.load_profile_map[i] = load_profile.LoadProfile().get_load_profile()
 
+            for n in range(self.timesteps):
+                load = self.get_load(i, n)
+                load.P = self.load_profile_map[i][n] / (self.network[n].base_power * 1e6) # convert to p.u.
+
+    def generate_solar_profiles(self):
+        for i in range(self.network[0].num_gens):
+            self.solar_profile_map[i] = solar_profile.SolarProfile().get_generation_profile()
+
+            for n in range(self.timesteps):
+                generator = self.get_gen(i, n)
+                generator.P = self.solar_profile_map[i][n] / (self.network[n].base_power * 1e6) # convert to p.u.
+
+
+    def get_network_for_time(self, timestep):
+        return self.network[0]
 
