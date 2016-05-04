8ac2f89b0dc3c80bc69edccf0837ebd1861bc050
Martin Zellner <martin.zellner@martinzellner.eu>
2016-05-04 13:39:37 +0200

    take average of 60 min load profile

--- mppfnet/load_profile.py
+++ mppfnet/load_profile.py
@@ -1,18 +1,19 @@
 from alpg.households import HouseholdSingleWorker
+import numpy as np
 
 class LoadProfile():
 
     def __init__(self):
         pass
 
-    def get_generation_profile(self, start_date=None, end_date=None):
+    def get_load_profile(self, start_date=None, end_date=None):
         h = HouseholdSingleWorker()
         h.simulate()
         h.scaleProfile()
         data = h.Consumption['Total']
         hourly = []
         for i in range(0, len(data), 60):
-            hourly.append(sum(data[i:i+60]))
+            hourly.append(np.mean(data[i:i+60]))
 
         if start_date is None and end_date is None:
             return hourly
