5208a6adc4fd86d3b51767ec48840b011fafd316
Martin Zellner <martin.zellner@martinzellner.eu>
2016-05-05 17:20:18 +0200

    Multi-Period PF without time coupling

--- mppfnet/mp_network.py
+++ mppfnet/mp_network.py
@@ -1,11 +1,17 @@
-import pfnet
 from datetime import datetime
+
+import numpy as np
+import pfnet
+import scipy.sparse
+
 from . import load_profile
 from . import solar_profile
+
 
 class MPNetwork():
 
     network = dict()
+    problem = dict()
     timesteps = 8760
     start_date = datetime.strptime("1.1.2016", "%d.%m.%Y")
     end_date = datetime.strptime("1.1.2017", "%d.%m.%Y")
@@ -76,3 +82,61 @@ class MPNetwork():
     def get_network_for_time(self, timestep):
         return self.network[0]
 
+    def construct_problem(self):
+        self.construct_subproblems()
+        A = scipy.sparse.block_diag([self.problem[i].A for i in range(self.timesteps)])
+
+        x = np.vstack([np.array(self.problem[i].x, ndmin=2).transpose() for i in range(self.timesteps)])
+        b = np.vstack([np.array(self.problem[i].b, ndmin=2).transpose() for i in range(self.timesteps)])
+        f = []
+        for i in range(self.timesteps):
+            f += self.problem[i].functions
+        return A, x, b, f
+
+    def construct_subproblems(self):
+        for timestep in range(self.timesteps):
+            p = pfnet.Problem()
+            p.set_network(self.network[timestep])
+
+            self.network[timestep].clear_flags()
+
+            # bus voltage angles
+            self.network[timestep].set_flags(pfnet.OBJ_BUS,
+                                             pfnet.FLAG_VARS,
+                                             pfnet.BUS_PROP_NOT_SLACK,
+                                             pfnet.BUS_VAR_VANG)
+
+            # slack gens active powers
+            self.network[timestep].set_flags(pfnet.OBJ_GEN,
+                                             pfnet.FLAG_VARS,
+                                             pfnet.GEN_PROP_SLACK,
+                                             pfnet.GEN_VAR_P)
+
+            p.add_constraint(pfnet.CONSTR_TYPE_DCPF)  # power flow
+            p.add_constraint(pfnet.CONSTR_TYPE_PAR_GEN_P)  # generator participation
+            p.add_constraint(pfnet.CONSTR_TYPE_PAR_GEN_Q)  # generator participation
+            p.analyze()
+            x = p.get_init_point()
+            p.eval(x)
+
+            self.problem[timestep] = p
+
+    def eval(self, x):
+        for i in range(self.timesteps):
+            m = len(self.problem[i].x)
+            self.problem[i].eval(x.transpose().flatten()[i * m:i * m + m])
+
+        return np.vstack([np.array(self.problem[i].x, ndmin=2).transpose() for i in range(self.timesteps)])
+
+    def update_properties(self, x):
+        for i in range(self.timesteps):
+            m = len(self.problem[i].x)
+            self.network[i].update_properties(x.transpose().flatten()[i * m:i * m + m])
+
+    def get_battery_constraint(self, battery):
+        """
+        returns the rows of the A matrix representing the battery constraints of the given battery object.
+        :param battery: The battery object the constraints are for
+        :return: The rows that are added to the A matrix
+        """
+        pass
